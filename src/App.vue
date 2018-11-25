4dee7447dc579d10f103fc916f4a88787be7cb2a
NikitaLem <saintsuigintoufag@gmail.com>
2018-11-25 13:05:13 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div style="display: flex; justify-content: space-around;">
+      <div>
+        <h2>Modal-range-mode(default)</h2>
+        <ss-date-time-picker
+          v-model="choosenDate"
+          :active.sync="datePickerIsVisible"
+        ></ss-date-time-picker>
+        <input type="text" id="modal-date-picker" @focus="showDatePicker" :value="inputText">
+      </div>
+      <div>
+        <h2>Inline-range-mode</h2>
+        <ss-date-time-picker
+          :id=1
+          :active=true
+          :is-modal=false
+        ></ss-date-time-picker>
+      </div>
+      <div>
+        <h2>Inline-single-mode</h2>
+        <ss-date-time-picker
+          :id=2
+          :active=true
+          :is-modal=false
+          :single-mode=true
+        ></ss-date-time-picker>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+<script>
+import VueDateTimePicker from 'ss-vue-datetime-range-picker';
+require('ss-vue-datetime-range-picker/dist/ss-vue-datetime-range-picker.css');
+
+export default {
+  name: 'App',
+  components: {
+    'ss-date-time-picker': VueDateTimePicker,
+  },
+  data() {
+    return {
+      datePickerIsVisible: false,
+      choosenDate: {},
+    };
+  },
+  computed: {
+    inputText() {
+      return moment.unix(this.choosenDate.fromTs).format('DD.MM.YYYY hh:mm') + '-'
+        + moment.unix(this.choosenDate.toTs).format('DD.MM.YYYY hh:mm');
+    },
+  },
+  methods: {
+    showDatePicker() {
+      this.datePickerIsVisible = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  margin-top: 60px;
+}
+</style>
